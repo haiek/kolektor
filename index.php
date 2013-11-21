d0cb82613869e4cb7145554b1d5326ee8eb1b08e
freddetastic <fredde@fredriks-mbp.dhcp.hyperisland.local>
2013-11-21 14:27:16 +0100

    Fixing a few issues.

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 
 <head>
 
-	<title>Radio Kolektor</title>
+	<title>Kolektor</title>
 
-	<!-- Metas -->
-    <meta content="minimum-scale=1.0, width=device-width, maximum-scale=1, user-scalable=no" name="viewport" />
-    <meta charset="utf-8">  
-    <meta name="apple-mobile-web-app-capable" content="yes" />          
-    <meta name="apple-mobile-web-app-status-bar-style" content="black"> 
+	<!-- METAS -->
+	<meta content="minimum-scale=1.0, width=device-width, maximum-scale=1, user-scalable=no" name="viewport" />
+	<meta charset="utf-8">  
+	<meta name="apple-mobile-web-app-capable" content="yes" />          
+	<meta name="apple-mobile-web-app-status-bar-style" content="black"> 
 
     <!-- CSS -->
     <link href="css/style.css" rel="stylesheet" media="screen">
